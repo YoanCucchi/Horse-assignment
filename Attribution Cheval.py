a0e7eb244a3f2ebae791a498bf8e99fa60f93f6f
Yoan Cucchi <yoan066@yahoo.fr>
2023-05-13 15:45:14 +0200

    excel automated

--- Attribution Cheval.py
+++ Attribution Cheval.py
@@ -2,24 +2,24 @@ import random
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox, simpledialog
 from collections import defaultdict
+import openpyxl
 
 global semaines
 # un cavalier n'aura pas le meme cheval pendant X semaines
 semaines = 6
 
 def ajouter_manuellement():
-	historique = lire_historique(historique_file.get())
+	historique = lire_historique(historique_file)
 	cavalier = simpledialog.askstring("Ajouter manuellement", "Entrez le nom du cavalier:")
 	cheval = simpledialog.askstring("Ajouter manuellement", "Entrez le nom du cheval:")
 	
 	if cavalier and cheval:
-		print(f'cavalier = {cavalier}')
 		if cavalier not in historique:
 			historique[cavalier] = [cheval]
 		else:
 			historique[cavalier].append(cheval)
 		messagebox.showinfo("Succès", f"{cheval} a été ajouté manuellement pour {cavalier} dans l'historique.")
-		with open(historique_file.get(), 'w') as f:
+		with open(historique_file, 'w') as f:
 			for cavalier, chevaux in historique.items():
 				f.write(f'{cavalier}:{",".join(chevaux)}\n')
 	else:
@@ -58,7 +58,6 @@ def candidats_pour_cavalier(cavalier, semaines_restriction):
 	for cheval_precedent in historique.get(cavalier[0], [])[-semaines_restriction:]:
 		if cheval_precedent in candidats:
 			candidats.remove(cheval_precedent)
-
 	return candidats
 
 def attribution(chevaux, cavaliers, historique):
@@ -72,10 +71,9 @@ def attribution(chevaux, cavaliers, historique):
 		cavalier, candidats = cavaliers_tries[0]
 		cavalier_nom, cavalier_taille, cavalier_jour, cavalier_créneau = cavalier
 
-
 		if candidats:
 			cheval_attribue = random.choice(candidats)
-			attribution_courante[cavalier_nom] = cheval_attribue
+			attribution_courante[cavalier_nom, cavalier_jour] = cheval_attribue, cavalier_créneau
 			chevaux.remove([cheval for cheval in chevaux if cheval[0] == cheval_attribue][0])
 			cavaliers_restants.remove(cavalier)
 		else:
@@ -83,30 +81,26 @@ def attribution(chevaux, cavaliers, historique):
 			candidats_semaines_moins_2 = candidats_pour_cavalier(cavalier, semaines - 2)
 			if candidats_semaines_moins_1:
 				cheval_attribue = random.choice(candidats_semaines_moins_1)
-				attribution_courante[cavalier_nom] = cheval_attribue
+				attribution_courante[cavalier_nom, cavalier_jour] = cheval_attribue, cavalier_créneau
 				chevaux.remove([cheval for cheval in chevaux if cheval[0] == cheval_attribue][0])
 				cavaliers_restants.remove(cavalier)
 			elif candidats_semaines_moins_2:
 				cheval_attribue = random.choice(candidats_semaines_moins_2)
-				attribution_courante[cavalier_nom] = cheval_attribue
+				attribution_courante[cavalier_nom, cavalier_jour] = cheval_attribue, cavalier_créneau
 				chevaux.remove([cheval for cheval in chevaux if cheval[0] == cheval_attribue][0])
 				cavaliers_restants.remove(cavalier)
 			else:
-				print(f"AUCUN CHEVAL ATTRIBUE pour {cavalier_nom} !!!!")
+				messagebox.showerror("Erreur", "AUCUN CHEVAL ATTRIBUE pour {} !!!!".format(cavalier_nom))
 				cavaliers_restants.remove(cavalier)
 
 	return attribution_courante
 
-
-
-
-
 def sauvegarder_historique(historique, attribution_courante):
 	for cavalier, cheval in attribution_courante.items():
-		if cavalier not in historique:
-			historique[cavalier] = [cheval]
+		if cavalier[0] not in historique:
+			historique[cavalier[0]] = [cheval[0]]
 		else:
-			historique[cavalier].append(cheval)
+			historique[cavalier[0]].append(cheval[0])
 
 	with open('historique.txt', 'w') as f:
 		for cavalier, chevaux in historique.items():
@@ -127,7 +121,8 @@ def browse_file(type):
 
 def load_cavaliers():
 	global cavaliers
-	cavaliers = lire_fichier(cavaliers_file.get())
+	cavaliers = lire_fichier(cavaliers_file)
+	load_button.destroy()
 	display_cavaliers()
 
 def display_cavaliers():
@@ -182,56 +177,62 @@ def display_cavaliers():
 		if jour_col < len(sorted_jours) - 1:
 			vertical_line = tk.Canvas(cavalier_frame, bg="gray", width=2, height=450)
 			vertical_line.grid(row=0, column=(jour_col + 1) * 9 - 1, rowspan=100)
-
 		row_index = 0
-
 
 def main():
 	global chevaux, cavaliers, historique
-	chevaux = lire_fichier(chevaux_file.get())
+	chevaux = lire_fichier(chevaux_file)
 
 	try:
-		historique = lire_historique(historique_file.get())
+		historique = lire_historique(historique_file)
 	except FileNotFoundError:
 		historique = {}
 
 	cavaliers_a_traiter = [c for c, var in zip(displayed_cavaliers, cavalier_vars) if var.get()]
 	attribution_courante = attribution(chevaux, cavaliers_a_traiter, historique)
+	sauvegarder_noms_cavaliers(attribution_courante)
 	display_attribution(attribution_courante)
 	sauvegarder_historique(historique, attribution_courante)
 
 def display_attribution(attribution_courante):
 	tree.delete(*tree.get_children())
 	for cavalier, cheval in attribution_courante.items():
-		tree.insert("", "end", values=(cavalier, cheval))
+		tree.insert("", "end", values=(cavalier[0], cheval[0]))
+
+def sauvegarder_noms_cavaliers(attribution_courante):
+	# Charger le fichier Excel existant
+	wb = openpyxl.load_workbook('feuille de monte.xlsx')
+
+	# Sélectionner la feuille de calcul à modifier
+	for cavalier, cheval in attribution_courante.items():
+		if "Mercredi" in cavalier[1]:
+			sheet = wb['Mercredi']
+		else:
+			sheet = wb['Samedi']
+		# Trouver la ligne et la colonne correspondantes
+		for row in range(1, sheet.max_row+1):
+			if sheet.cell(row=row, column=1).value == cheval[0]:
+				for col in range(1, sheet.max_column+1):
+				#print(f'{sheet.cell(row=row, column=col).value}')
+					if sheet.cell(row=1, column=col).value == cheval[1]:
+						# Écrire le nom du cavalier dans la cellule correspondante
+						sheet.cell(row=row, column=col).value = cavalier[0]
+	# Enregistrer les modifications dans le fichier Excel
+	wb.save('feuille de monte.xlsx')
 
 app = tk.Tk()
 app.title("Attribution Chevaux-Cavaliers")
 
-chevaux_file = tk.StringVar()
-cavaliers_file = tk.StringVar()
-historique_file = tk.StringVar()
+chevaux_file = 'chevaux.txt'
+cavaliers_file = 'cavaliers.txt'
+historique_file = 'historique.txt'
+
 cavalier_vars = []
 displayed_cavaliers = []
 
-chevaux_file.set('chevaux.txt')
-cavaliers_file.set('cavaliers.txt')
-historique_file.set('historique.txt')
-
-tk.Label(app, text="Fichier Chevaux:").grid(row=0, column=0)
-tk.Entry(app, textvariable=chevaux_file).grid(row=0, column=1)
-tk.Button(app, text="Parcourir", command=lambda: chevaux_file.set(browse_file("chevaux"))).grid(row=0, column=2)
-
-tk.Label(app, text="Fichier Cavaliers:").grid(row=1, column=0)
-tk.Entry(app, textvariable=cavaliers_file).grid(row=1, column=1)
-tk.Button(app, text="Parcourir", command=lambda: cavaliers_file.set(browse_file("cavaliers"))).grid(row=1, column=2)
-tk.Button(app, text="Charger", command=load_cavaliers).grid(row=1, column=3)
-
-tk.Label(app, text="Fichier Historique:").grid(row=2, column=0)
-tk.Entry(app, textvariable=historique_file).grid(row=2, column=1)
-tk.Button(app, text="Parcourir", command=lambda: historique_file.set(browse_file("historique"))).grid(row=2, column=2)
-
-tk.Button(app, text="Ajouter manuellement", command=ajouter_manuellement).grid(row=1, column=4, columnspan=2)
+load_button = tk.Button(app, text="Charger", command=load_cavaliers)
+load_button.grid(row=4, column=3)
+tk.Button(app, text="Ajouter manuellement", command=ajouter_manuellement).grid(row=6, column=4, columnspan=4, sticky="we")
 
 app.columnconfigure(0, weight=1, minsize=0)
 app.columnconfigure(1, weight=0, minsize=0)
@@ -244,14 +245,14 @@ app.columnconfigure(7, weight=0, minsize=0)
 
 cavalier_frame = tk.Frame(app)
 cavalier_frame.configure(height=500)
-cavalier_frame.grid(row=4, column=0, columnspan=5, pady=10, padx=0)
+cavalier_frame.grid(row=4, column=0, columnspan=5, pady=0, padx=0)
 
-tk.Button(app, text="Attribuer", command=main).grid(row=5, column=0, columnspan=5)
+tk.Button(app, text="Attribuer", command=main).grid(row=5, column=2, columnspan=2, sticky="we")
 
-tree = ttk.Treeview(app, columns=("Cavalier", "Cheval"), show="headings")
+tree = ttk.Treeview(app, columns=("Cavalier", "Cheval"), show="headings", height=15)
 tree.heading("Cavalier", text="Cavalier")
 tree.heading("Cheval", text="Cheval")
-tree.grid(row=6, column=0, columnspan=5, pady=10)
+tree.grid(row=6, column=0, columnspan=5, pady=1)
 
 app.protocol("WM_DELETE_WINDOW", app.quit)
 app.mainloop()
